3f84e4ce5bddc54ee01ba8521509f3f2cf6f07b6
29Tatiana <amiravob@mail.ru>
2023-06-11 13:38:42 +0300

    first commit

---
+++ Node.java
@@ -0,0 +1,14 @@
+
+public class Node {
+    
+    int value;
+    Node leftChild;
+    Node rightChild;
+    Color color;
+
+    public Node(int value) {
+        this.value = value;
+        this.color = Color.RED;
+    }
+
+}
